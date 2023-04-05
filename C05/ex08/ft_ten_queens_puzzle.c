841935331f04d56ec00ee46cacd38c7fa402aa3c
Sin Liang Lee <sinlee@u82z05s03.42kl.edu.my>
2023-04-05 20:07:42 +0800

    initial commiot

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sinlee <sinlee@student42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/05 20:07:18 by sinlee            #+#    #+#             */
+/*   Updated: 2023/04/05 20:07:32 by sinlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdbool.h>
 #include <unistd.h>
 
-// check diagonals
+// check for diagonals
 bool	is_valid(int *position, int c)
 {
 	int	i;
