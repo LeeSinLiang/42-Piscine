97f8fcc302e626087b502a1e22c137a29bf0d770
Sin Liang Lee <sinlee@u82z05s03.42kl.edu.my>
2023-04-05 20:06:36 +0800

    initial commit

---
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <unistd.h>
+
+bool	is_valid(int position, int c)
+{
+	int	i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (position == c)
+			return (false);
+	}
+	// check for diagonals
+}
+
+int	ft_ten_queens_puzzle(void)
+{
+}
