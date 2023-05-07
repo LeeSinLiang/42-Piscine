d61fd094286bc508f8f5167d4533c9c1a109d6d5
LeeSinLiang <mangojamlee@gmail.com>
2023-05-07 22:04:55 +0800

    initial commit

---
+++ rush00/main.c
@@ -0,0 +1,17 @@
+void ft_putchar(char c);
+
+void	rush(int n, int m);
+
+int	main(void)
+{
+	rush(5,3);
+	ft_putchar('\n');
+	rush(5,1);
+	ft_putchar('\n');
+	rush(1,1);
+	ft_putchar('\n');
+	rush(1,5);
+	ft_putchar('\n');
+	rush(4, 4);
+	return (0);
+}
---
+++ rush00/rush03.c
@@ -0,0 +1,79 @@
+void	ft_putchar(char c);
+
+void	firstline(int n, char start, char mid, char end);
+void	midline(int n, char openandend, char mid);
+void	lastline(int n, char start, char mid, char end);
+
+void	rush(int n, int m)
+{
+	int	i;
+
+	i = 0;
+	if (n >= 1)
+	{
+		firstline(n, 'A', 'B', 'C');
+		ft_putchar('\n');
+		if (m > 1)
+		{
+			while (i < m - 2)
+			{
+				midline(n, 'B', ' ');
+				ft_putchar('\n');
+				++i;
+			}
+			lastline(n, 'A', 'B', 'C');
+			ft_putchar('\n');
+		}
+	}
+}
+
+void	firstline(int n, char start, char mid, char end)
+{
+	int	i;
+
+	i = 0;
+	ft_putchar(start);
+	if (n != 1)
+	{
+		while (i < n - 2)
+		{
+			ft_putchar(mid);
+			++i;
+		}
+		ft_putchar(end);
+	}
+}
+
+void	midline(int n, char openandend, char mid)
+{
+	int	i;
+
+	if (n != 1)
+	{
+		i = 0;
+		ft_putchar(openandend);
+		while (i < n - 2)
+		{
+			ft_putchar(mid);
+			++i;
+		}
+	}
+	ft_putchar(openandend);
+}
+
+void	lastline(int n, char start, char mid, char end)
+{
+	int	i;
+
+	ft_putchar(start);
+	if (n != 1)
+	{
+		i = 0;
+		while (i < n - 2)
+		{
+			ft_putchar(mid);
+			++i;
+		}
+		ft_putchar(end);
+	}
+}
